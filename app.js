aca1f28991c11b6511d66b4672bce5af3e23e0dd
mikel <mikelbts89@gmail.com>
2020-11-05 21:56:10 +0200

    add_some_function_only_for_test

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
 console.log(true);
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+let doubelNumbers = (num) => {
+  let newNumbersArr = num.map((a) => {
+    return a * 2;
+  });
+  return newNumbersArr;
+};
+
+let newArr = doubelNumbers(numbers);
+
+console.log(newArr)
